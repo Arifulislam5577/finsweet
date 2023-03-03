cf7c6248694f8558cc244080ff8b347e06f97af7
Arif5577 <arifalamm33@gmail.com>
2023-03-03 10:35:30 +0600

    Not Found Page Added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/shared/Layout";
 import HomePage from "../pages/Home/Index";
+import NotFound from "../pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
