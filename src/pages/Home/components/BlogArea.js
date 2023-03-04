b0449035442766448392ff60d4f85e38263040da
Arif5577 <arifalamm33@gmail.com>
2023-03-04 10:19:41 +0600

    favicon added

--- src/pages/Home/components/BlogArea.js
+++ src/pages/Home/components/BlogArea.js
@@ -7,7 +7,7 @@ const BlogArea = () => {
     <div>
       <div className="container">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-stretch justify-between">
-          <div className="md:col-span-2 lg:col-span-1 w-full bg-lightGray flex flex-col justify-between">
+          <div className="md:col-span-1 lg:col-span-1 w-full bg-lightGray flex flex-col justify-between">
             <div className="p-8">
               <h1 className="text-3xl font-bold text-black mb-3">
                 Read our <br /> articles & news
